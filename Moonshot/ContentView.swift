a9a38d897960c89f5ebf54705af1eae8664a2e77
mleers <mleers4@gmail.com>
2022-10-29 16:04:32 -0700

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by mitch on 10/29/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,19 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by mitch on 10/29/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
